92c16f9a4383fd91d51721ef0358da4e31166152
blycr <87604710+blycr@users.noreply.github.com>
2021-08-18 10:51:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,3 +25,4 @@ sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packa
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
 #git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
